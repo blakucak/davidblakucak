a0b89a963875ed03112c895bb1f14e17d1a7a9cf
blakucak <david.blakucak@gmail.com>
2016-04-30 00:38:51 +0700

    Update con.js

--- routes/con.js
+++ routes/con.js
@@ -2,8 +2,8 @@ var db = function localConnect(){
     return require('mysql').createConnection({
         hostname: 'ap-cdbr-azure-southeast-b.cloudapp.net',
         user: 'bf666ccbb8ddd8',
-        password: 'pamaloppa',
-        database: '340ef08e'
+        password: '340ef08e',
+        database: 'file_sharing'
     });
     connection.connect();
 }
